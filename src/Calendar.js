439fc46a228d73b7b0123c4669703363677930ba
Casper Kuijjer <casper@kuijjer.com>
2018-09-10 08:15:47 +0200

    Initial styling

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import { DateTime } from 'luxon'
+import styled from 'react-emotion'
 import JSONStringify from './JSONStringify'
-import Log from './Log'
-
 import { screenings } from './data'
 
-const Calendar = () => {
-  // sort the screenings by date, then render
-  const enhancedScreenings = screenings
-    .map(x => ({ ...x, date: DateTime.fromISO(x.date) })) // use luxon on the date
-    .sort((a, b) => a.date - b.date) // sort by date ascending
+const Screening = styled('div')({})
 
-  return <JSONStringify>{{ screenings, enhancedScreenings }}</JSONStringify>
-}
+const Time = styled('div')({
+  fontSize: 24,
+})
+
+const Title = styled('div')({
+  fontSize: 24,
+})
+
+const Cinema = styled('div')({
+  fontSize: 24,
+})
+
+// sort the screenings by date, then render
+const enhancedScreenings = screenings
+  .map(x => ({ ...x, date: DateTime.fromISO(x.date) })) // use luxon on the date
+  .sort((a, b) => a.date - b.date) // sort by date ascending
+
+const Calendar = () => (
+  <>
+    {enhancedScreenings.map(screening => (
+      <Screening>
+        <Time>{screening.date.toLocaleString(DateTime.TIME_24_SIMPLE)}</Time>
+        <Title>{screening.title}</Title>
+        <Cinema>{screening.cinema}</Cinema>
+      </Screening>
+    ))}
+    <JSONStringify>{{ screenings, enhancedScreenings }}</JSONStringify>
+  </>
+)
 
 export default Calendar
