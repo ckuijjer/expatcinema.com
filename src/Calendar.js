b71d487cd99cc0402e6242997350bb4c284f7952
Casper Kuijjer <casper@kuijjer.com>
2018-09-11 08:52:27 +0200

    Remove Log

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,7 +3,6 @@ import { DateTime } from 'luxon'
 import styled from 'react-emotion'
 import { groupBy } from 'ramda'
 import JSONStringify from './JSONStringify'
-import Log from './Log'
 import { screenings } from './data'
 
 const Screening = styled('div')({
