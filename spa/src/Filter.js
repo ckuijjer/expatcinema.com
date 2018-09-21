30f2231dc243b341bca6082a931f75ee576b8c9b
Casper Kuijjer <casper@kuijjer.com>
2018-09-21 17:43:02 +0200

    Add a non working filter

---
+++ spa/src/Filter.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Filter = () => (
+  <input  placeholder="Type to search" style={{ padding: 12, width: '100%', fontSize: 20 }} autoFocus />
+)
+
+export default Filter
