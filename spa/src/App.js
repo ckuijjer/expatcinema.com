d699d9a31840920310d549a21b3921f3453f66e7
Casper Kuijjer <casper@kuijjer.com>
2018-09-21 17:22:58 +0200

    Simplify

--- spa/src/App.js
+++ spa/src/App.js
@@ -4,35 +4,30 @@ import About from './About'
 import Calendar from './Calendar'
 import Header from './Header'
 
-const Container = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-})
+const Container = styled('div')({})
 
 const Content = styled('div')({
-  marginLeft: 20,
-  marginRight: 20,
-  maxWidth: 900,
-})
+  margin: '0 auto',
 
-const AboutContainer = styled('div')({
-  backgroundColor: '#fbfbfb',
   paddingLeft: 20,
   paddingRight: 20,
-  alignSelf: 'stretch',
+  maxWidth: 960,
 })
 
 const App = () => (
-  <Container>
-    <Content>
-      <Header />
-      <Calendar />
-    </Content>
-    <AboutContainer>
-      <About />
-    </AboutContainer>
-  </Container>
+  <>
+    <Container>
+      <Content>
+        <Header />
+        <Calendar />
+      </Content>
+    </Container>
+    <Container style={{ backgroundColor: '#fbfbfb' }}>
+      <Content>
+        <About />
+      </Content>
+    </Container>
+  </>
 )
 
 export default App
