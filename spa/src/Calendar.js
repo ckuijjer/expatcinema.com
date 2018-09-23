5819815e6c5afaad93d6b713bf4cb158bee1af5a
Casper Kuijjer <casper@kuijjer.com>
2018-09-23 18:23:34 +0200

    react-virtualized (but broken About)

--- spa/src/Calendar.js
+++ spa/src/Calendar.js
@@ -1,83 +1,91 @@
 import React from 'react'
 import { DateTime } from 'luxon'
-import styled from 'react-emotion'
+import { AutoSizer, List, WindowScroller } from 'react-virtualized'
 
-import JSONStringify from './JSONStringify'
 import RelativeDate from './RelativeDate'
-import Time from './Time'
+import Screening from './Screening'
 import screenings from './screenings'
-
-const Screening = styled('div')({
-  display: 'grid',
-  gridTemplateColumns: '[time] 60px [rest] auto',
-  gridColumnGap: 12,
-  lineHeight: 1.4,
-  padding: 10,
-})
-
-const Title = styled('div')({
-  fontSize: 20,
-})
-
-const Cinema = styled('div')({
-  fontSize: 16,
-  gridColumnStart: 'rest',
-  color: '#777',
-})
-
-const A = styled('a')({
-  display: 'block',
-  textDecoration: 'none',
-  color: '#333',
-  marginLeft: -10,
-  marginRight: -10,
-
-  ':hover': {
-    backgroundColor: '#f6f6f6',
-    borderRadius: 10,
-  },
-})
-
-const DaySection = styled('div')({
-  marginBottom: 40,
-})
 
 const screeningsByDate = Object.entries(screenings).sort(([a], [b]) => {
   return DateTime.fromISO(a) - DateTime.fromISO(b)
 }) // sort by date
 
-const Calendar = ({ cities }) => (
-  <>
-    {screeningsByDate.map(([date, screenings]) => {
+const flatten = (acc, cur) => [...acc, ...cur]
+
+const Calendar = ({ cities }) => {
+  const rows = screeningsByDate
+    .map(([date, screenings]) => {
       const filteredScreenings = screenings.filter(
         screening =>
           cities.length === 0 || cities.includes(screening.cinema.city),
       )
-
       if (filteredScreenings.length) {
-        return (
-          <DaySection key={date}>
-            <RelativeDate>{date}</RelativeDate>
-            {filteredScreenings.map(screening => (
-              <A href={screening.url}>
-                <Screening>
-                  <Time>{screening.date}</Time>
-                  <Title>{screening.title}</Title>
-                  <Cinema>
-                    {screening.cinema.name}
-                    <br />
-                    {screening.cinema.city}
-                  </Cinema>
-                </Screening>
-              </A>
-            ))}
-          </DaySection>
-        )
-      } else {
-        return null
+        return [date, filteredScreenings]
       }
-    })}
-  </>
-)
+    })
+    .filter(x => x)
+    .map(([date, screenings]) => {
+      return [
+        { component: 'RelativeDate', props: { children: date } },
+        ...screenings.map(screening => ({
+          component: 'Screening',
+          props: screening,
+        })),
+      ]
+    })
+    .reduce(flatten, [])
+
+  // function rowRenderer ({
+  //   index,       // Index of row
+  //   isScrolling, // The List is currently being scrolled
+  //   isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+  //   key,         // Unique key within array of rendered rows
+  //   parent,      // Reference to the parent List (instance)
+  //   style        // Style object to be applied to row (to position it);
+  //                // This must be passed through to the rendered row element.
+  // }) {
+  const renderRow = ({ index, key, style }) => {
+    const row = rows[index]
+
+    if (row.component === 'RelativeDate') {
+      return (
+        <div key={key} style={style}>
+          <RelativeDate {...row.props} />
+        </div>
+      )
+    } else {
+      return (
+        <div key={key} style={style}>
+          <Screening {...row.props} />
+        </div>
+      )
+    }
+  }
+
+  const getRowHeight = ({ index }) =>
+    rows[index].component === 'RelativeDate' ? 60 : 100
+
+  return (
+    <WindowScroller>
+      {({ height, isScrolling, onChildScroll, scrollTop }) => (
+        <AutoSizer style={{ height }}>
+          {({ width }) => (
+            <List
+              autoHeight
+              height={height}
+              isScrolling={isScrolling}
+              onScroll={onChildScroll}
+              rowCount={rows.length}
+              rowHeight={getRowHeight}
+              rowRenderer={renderRow}
+              scrollTop={scrollTop}
+              width={width}
+            />
+          )}
+        </AutoSizer>
+      )}
+    </WindowScroller>
+  )
+}
 
 export default Calendar
