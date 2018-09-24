6f8c427bd336d7384cdc60a2659b180a6ddad062
Casper Kuijjer <casper@kuijjer.com>
2018-09-24 18:46:46 +0200

    Use of react-easy-state and react-easy-params

--- spa/src/App.js
+++ spa/src/App.js
@@ -17,40 +17,22 @@ const Content = styled('div')({
   maxWidth: 960,
 })
 
-class App extends React.Component {
-  state = {
-    search: '',
-    cities: [],
-  }
-
-  handleSearch = e => {
-    this.setState({ search: e.target.value })
-  }
-
-  handleCityChange = cities => this.setState(state => ({ cities }))
-
-  render() {
-    return (
-      <>
-        <Container>
-          <Content>
-            <Header />
-            <TextFilter
-              value={this.state.search}
-              onChange={this.handleSearch}
-            />
-            <CityFilter onChange={this.handleCityChange} />
-            <Calendar cities={this.state.cities} search={this.state.search} />
-          </Content>
-        </Container>
-        {/* <Container style={{ backgroundColor: '#fbfbfb' }}>
+const App = () => (
+  <>
+    <Container>
+      <Content>
+        <Header />
+        <TextFilter />
+        <CityFilter />
+        <Calendar />
+      </Content>
+    </Container>
+    {/* <Container style={{ backgroundColor: '#fbfbfb' }}>
           <Content>
             <About />
           </Content>
         </Container> */}
-      </>
-    )
-  }
-}
+  </>
+)
 
 export default App
