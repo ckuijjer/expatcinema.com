3bd636c71f84ffc7417b22b4612e75ddba2b2dd5
Casper Kuijjer <casper@kuijjer.com>
2018-09-25 17:46:31 +0200

    Added a Cross to clear

---
+++ spa/src/Cross.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Cross = () => (
+  <svg
+    width="32px"
+    height="32px"
+    viewBox="0 0 32 32"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g
+      id="Artboard"
+      stroke="none"
+      strokeWidth="1"
+      fill="none"
+      fillRule="evenodd"
+      strokeLinecap="square"
+    >
+      <path d="M8,8 L24,24" id="Line" stroke="#0650D0" strokeWidth="3" />
+      <path d="M24,8 L8,24" id="Line-2" stroke="#0650D0" strokeWidth="3" />
+    </g>
+  </svg>
+)
+
+export default Cross
