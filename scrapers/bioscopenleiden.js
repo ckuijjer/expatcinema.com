57bb18cb4ba37717df07a3da29eec3aacb100c3c
Casper Kuijjer <casper@kuijjer.com>
2018-10-06 11:19:44 +0200

    Fixed bioscopenleiden (can handle oktober now)

--- scrapers/bioscopenleiden.js
+++ scrapers/bioscopenleiden.js
@@ -1,5 +1,4 @@
 const Xray = require('x-ray')
-const xray = Xray()
 const R = require('ramda')
 const { DateTime } = require('luxon')
 
@@ -9,6 +8,65 @@ const debugPromise = (format, ...debugArgs) => arg => {
   debug(format, ...debugArgs, arg)
   return arg
 }
+const xray = Xray()
+  .concurrency(3)
+  .throttle(3, 300)
+
+const monthToNumber = month =>
+  [
+    'januari',
+    'februari',
+    'maart',
+    'april',
+    'mei',
+    'juni',
+    'juli',
+    'augustus',
+    'september',
+    'oktober',
+    'november',
+    'december',
+  ].indexOf(month) + 1
+
+const splitTime = time => time.split(':').map(x => Number(x))
+
+const extractFromMoviePage = ({ url }) =>
+  xray(url, 'body', [
+    {
+      title: '.h-title',
+      date: '.sub-date',
+      time: '.time',
+      cinema: '.building',
+    },
+  ])
+    .then(debugPromise('movie page'))
+    .then(results =>
+      results
+        .map(r => ({ ...r, url })) // add the movie page's url
+        .map(r => ({
+          ...r,
+          title: r.title.replace('Expat Cinema: ', ''), // Remove Expat Cinema from the title
+        }))
+        .map(r => {
+          const { date, time, ...rest } = r
+          const [dayString, monthString] = date.split(' ')
+          const day = Number(dayString)
+          const month = monthToNumber(monthString)
+          const [hour, minute] = splitTime(time)
+
+          return {
+            ...rest,
+            date: DateTime.fromObject({
+              day,
+              month,
+              hour,
+              minute,
+            })
+              .toUTC()
+              .toISO(),
+          }
+        }),
+    )
 
 const extractFromMainPage = () =>
   xray('https://www.bioscopenleiden.nl', '.schedule .tab-content li', [
@@ -23,41 +81,17 @@ const extractFromMainPage = () =>
         Promise.all(
           results
             .filter(x => x.title && x.title.startsWith('Expat Cinema'))
-            .map(x => {
-              return xray(x.url, 'body', [
-                {
-                  title: '.h-title',
-                  date: '.sub-date',
-                  time: '.time',
-                  cinema: '.building',
-                },
-              ])
-                .then(debugPromise('movie page'))
-                .then(results =>
-                  results
-                    .map(r => ({ ...r, url: x.url })) // add the movie page's url
-                    .map(r => ({
-                      ...r,
-                      title: r.title.replace('Expat Cinema: ', ''), // Remove Expat Cinema from the title
-                    }))
-                    .map(r => {
-                      const { date, time, ...rest } = r
-                      return {
-                        ...rest,
-                        date: DateTime.fromFormat(
-                          `${date} ${time}`,
-                          'd MMMM H:mm',
-                        )
-                          .toUTC()
-                          .toISO(), // create a iso8601 using the date and the time
-                      }
-                    }),
-                )
-            }),
+            .map(extractFromMoviePage),
         )
           .then(R.flatten)
           .then(R.uniq), // remove duplicates
     )
+
+if (require.main === module) {
+  extractFromMainPage()
+    .then(x => JSON.stringify(x, null, 2))
+    .then(console.log)
+}
 
 module.exports = extractFromMainPage
 
