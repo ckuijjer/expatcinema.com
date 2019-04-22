d55215dde7dd5e9d682a8d31b6b9b7ee471e9127
Casper Kuijjer <casper@kuijjer.com>
2019-04-22 10:52:22 +0200

    more slack notification reasons

--- cloud/notify-slack.js
+++ cloud/notify-slack.js
@@ -5,7 +5,13 @@ const zlib = require('zlib')
 // see https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/SubscriptionFilters.html#LambdaFunctionExample
 // aws logs put-log-events --log-group-name test --log-stream-name test --log-events "[{\"timestamp\":1555846737890 , \"message\": \"Simple Lam bda Test Task timed out after sdf\"}]" --sequence-token 49593243400316228378531638056594071833748229412712679042
 
-const filters = [/Task timed out after/]
+const filters = [
+  /Task timed out after/, // running the lambda function times out
+  /UnhandledPromiseRejectionWarning/, // a promise rejection isn't handled
+  /"date": null'/, // a movie is extracted with null date
+  /socket hang up/, // no content received within the timeout period
+  /with status code: (4|5)\d{2}/, // HTTP error code 4xx, 5xx received
+]
 
 const gunzip = util.promisify(zlib.gunzip)
 
