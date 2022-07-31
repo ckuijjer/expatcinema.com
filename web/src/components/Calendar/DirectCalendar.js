f2ae6cf9caa6a2ccc26b34e49280697babb37340
Casper Kuijjer <casper@kuijjer.com>
2022-07-31 20:31:18 +0200

    revert

--- web/src/components/Calendar/DirectCalendar.js
+++ web/src/components/Calendar/DirectCalendar.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import hash from 'object-hash'
-
 import RelativeDate from '../RelativeDate'
 import Screening from '../Screening'
 
 const DirectCalendar = ({ rows, showCity }) => (
   <>
-    {rows.map((row) => {
-      const key = hash(row)
-
-      return row.component === 'RelativeDate' ? (
-        <RelativeDate {...row.props} key={key} />
+    {rows.map((row) =>
+      row.component === 'RelativeDate' ? (
+        <RelativeDate {...row.props} />
       ) : (
-        <Screening {...row.props} showCity={showCity} key={key} />
-      )
-    })}
+        <Screening {...row.props} showCity={showCity} />
+      ),
+    )}
   </>
 )
 
