6bc566da1aff704be72d6464cc18c6a29b8b9ebb
Casper Kuijjer <casper@kuijjer.com>
2022-10-12 10:21:06 +0200

    trigger build

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 
 import App from '../components/App'
-import Layout from '../components/Layout'
-// import Seo from '../components/Seo'
 import { getScreenings } from '../components/getScreenings'
 import Seo from '../components/Seo'
 
