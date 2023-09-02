91ac3c02a1288ae542b60a5e2d49488de1935777
Casper Kuijjer <casper@kuijjer.com>
2023-09-02 09:29:52 +0200

    kriterion fixed

--- cloud/scrapers/kriterion.ts
+++ cloud/scrapers/kriterion.ts
@@ -1,8 +1,8 @@
-import Xray from 'x-ray'
 import { DateTime } from 'luxon'
 
 import { Screening } from '../types'
 import { logger as parentLogger } from '../powertools'
+import got from 'got'
 
 const logger = parentLogger.createChild({
   persistentLogAttributes: {
@@ -10,102 +10,145 @@ const logger = parentLogger.createChild({
   },
 })
 
-const xray = Xray({
-  filters: {
-    trim: (value) => (typeof value === 'string' ? value.trim() : value),
-    cleanTitle: (value) =>
-      typeof value === 'string'
-        ? value.replace(/ ENG SUBS$/, '').replace(' (ENG SUBS)', '')
-        : value,
-  },
-})
-  .concurrency(2)
-  .throttle(10, 300)
-  .timeout('5s')
-
-const hasEnglishSubtitles = ({ metadata }: XRayFromMoviePage) => {
-  const hasEnglishSubtitles =
-    metadata.includes('Ondertiteling Engels') ||
-    metadata.includes('Ondertiteling English')
-  logger.info('hasEnglishSubtitles', { hasEnglishSubtitles, metadata })
-  return hasEnglishSubtitles
+interface Show {
+  id: number
+  name: string
+  code: string
+  starts_at: string
+  pre_show_duration: number
+  post_show_duration: number
+  theatre_id: number
+  genre_id: number
+  ticket_product_id: number
+  category_id: number
+  festival_id: number
+  layout_id: number
+  external_url: string | null
+  content_actions: unknown | null
+  mid_show_duration: number
+  duration: number
+  default_seat_category_id: number
+  maccsbox_code: string
+  maccsbox_contract: unknown | null
+  maccsbox_show_type: unknown | null
+  media_type: unknown | null
+  digital_projection_type: unknown | null
+  is_3d: boolean
+  price_plan_id: number
+  must_create_customer: boolean
+  intro_duration: number
+  extro_duration: number
+  external_planner_id: unknown | null
+  production_id: number
+  is_for_cineville: boolean
+  is_cineville_reservation_blocked: boolean
+  customer_field_validators: unknown | null
+  max_sellable_tickets: number
+  wc_duration: number
+  wc_extro_duration: number
+  wc_festival_id: number
+  wc_max_sellable_tickets: number
+  wc_mid_show_duration: number
+  wc_intro_duration: number
+  wc_is_cineville_reservation_blocked: boolean
+  wc_is_for_api: boolean
+  wc_is_theatre_seated: boolean
+  wc_post_show_duration: number
+  wc_pre_show_duration: number
+  wc_price_plan_id: number
+  wc_ticket_product_id: number
+  start_date: string
+  start_time: string
+  display_name: string
+  theatre_name: string
+  is_a_set: boolean
+  ends_at: string
+  updated_at: string
+  customer_category_id: number
+  has_sold_tickets: boolean
+  has_reserved_or_sold_tickets: boolean
+  is_theatre_seated: string
+  genre_code: string
+  is_deleted: string
+  category_code: string
+  extra_title_info: unknown | null
+  director: string
+  spoken_languages: unknown | null
+  subtitle_languages: unknown | null
+  spoken_languages_alt: unknown | null
+  subtitle_languages_alt: unknown | null
+  has_donation: string
+  is_remote: string
+  ticket_text_long: unknown | null
+  ticket_text_short: unknown | null
+  attachment_label: unknown | null
+  attachment_url: unknown | null
+  after_sales_message: unknown | null
 }
 
-type XRayFromMoviePage = {
-  metadata: string[]
-  sidebar: {
-    title: string
-    url: string
-    date: string
-  }[]
+interface KriterionShowsApiResponse {
+  success: boolean
+  shows: Show[]
 }
 
-const extractFromMoviePage = async (url: string): Promise<Screening[]> => {
-  logger.info('extracting', { url })
-
-  const movie: XRayFromMoviePage = await xray(url, 'body', {
-    metadata: ['#filmposter p'], // iterate to get "Ondertiteling Engels"
-    sidebar: xray('li[typeof="schema:TheaterEvent"]', [
-      {
-        title: '[property="schema:name"] | trim | cleanTitle',
-        url: 'span[property="schema:url"] | trim', // as the <a> has a ?start_date appended
-        date: '[property="schema:startDate"]@datetime',
-      },
-    ]),
-  })
-
-  logger.info('extracted xray', { url, movie })
-
-  if (!hasEnglishSubtitles(movie)) return []
-
-  const screenings = movie.sidebar
-    .filter((x) => x.url === url) // only look at the sidebar items that are about the current movie
-    .map(({ date, ...rest }) => ({
-      ...rest,
-      cinema: 'Kriterion',
-      date: DateTime.fromISO(date).toJSDate(),
-    }))
-
-  logger.info('extracted', { url, screenings })
-
-  return screenings
+interface KriterionFilmsApiResponse {
+  [key: string]: number
 }
 
-type XRayFromMainPage = {
-  title: string
-  url: string
-  date: string
-}
+const cleanTitle = (title: string) => title.replace(/ \(ENG SUBS\)$/i, '')
 
-// for now we only scrape movies that can be bought, so unfortunately not the ones
-// a little more in the future (this will be harder to scrape as we need to parse the metadata which looks
-// only semi structured, thus fragile. So now scraping of the main content '.highlightbox h4' but only
-// the sidebar, and using the sidebar in extractFromMoviePage again so we only make one request for every
-// movie, but still can easily get all the screenings
 const extractFromMainPage = async () => {
   try {
-    const xrayResult: XRayFromMainPage[] = await xray(
-      'https://www.kriterion.nl/agenda-2-2-2-2',
-      'li[typeof="schema:TheaterEvent"]',
-      [
-        {
-          title: '[property="schema:name"] | trim',
-          url: 'span[property="schema:url"] | trim', // as the <a> has a ?start_date appended
-          date: '[property="schema:startDate"]@datetime',
-        },
-      ],
+    const showsApiResponse: KriterionShowsApiResponse = await got(
+      'https://storage.googleapis.com/kritsite-buffer/shows.json',
+    ).json()
+
+    const filmsApiResponse: KriterionFilmsApiResponse = await got(
+      'https://storage.googleapis.com/kritsite-buffer/films.json',
+    ).json()
+
+    logger.info('extracted api responses', {
+      showsApiResponse,
+      filmsApiResponse,
+    })
+
+    const productionIdToSlug = Object.entries(filmsApiResponse).reduce(
+      (acc, [slug, productionId]) => {
+        acc[productionId] = slug
+        return acc
+      },
+      {} as { [key: number]: string },
     )
 
-    logger.info('main page before', { xrayResult })
+    const screenings: Screening[] = showsApiResponse.shows
+      .filter((item) => item.name.includes('(ENG SUBS)'))
+      .map((item) => {
+        const slug = productionIdToSlug[item.production_id].replace(
+          /-eng-subs/,
+          '',
+        )
+        const url = `https://kriterion.nl/films/${slug}`
 
-    const uniqueUrls = Array.from(new Set(xrayResult.map((x) => x.url)))
+        const screening: Screening = {
+          title: cleanTitle(item.name),
+          url,
+          cinema: 'Kriterion',
+          date: DateTime.fromISO(item.starts_at).toJSDate(),
+        }
+        return screening
+      })
+      .filter((screening) => {
+        // filter out screenings without a url, and log them
+        if (screening.url === undefined) {
+          logger.warn('screening.url undefined', { screening })
+          return false
+        }
 
-    logger.info('main page after uniq', { uniqueUrls })
+        return true
+      })
 
-    const screenings = await Promise.all(uniqueUrls.map(extractFromMoviePage))
-    logger.info('before flatten', { screenings })
-
-    return screenings.flat()
+    logger.info('extracted screenings', { screenings })
+    return screenings
   } catch (error) {
     logger.error('error scraping kriterion', { error })
   }
