dc1086bd1e95a9ec7c8ef6986f22b359021b8a97
Casper Kuijjer <casper@kuijjer.com>
2023-09-03 14:28:13 +0200

    log error

--- cloud/xRayPuppeteer.ts
+++ cloud/xRayPuppeteer.ts
@@ -39,7 +39,7 @@ const xRayPuppeteer = ({
       logger?.info('done retrieving content', { url: ctx.url })
       done(null, ctx)
     } catch (error) {
-      logger?.error('error retrieving', { url: ctx.url })
+      logger?.error('error retrieving', { url: ctx.url, error })
       return done(error, null)
     } finally {
       logger?.info('closing browser')
