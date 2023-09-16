84f9cf2a5ebcb035bdaceae56508bf5f56eb5fa5
Casper Kuijjer <casper@kuijjer.com>
2023-09-16 16:44:04 +0200

    fix: lang=en by using custom document

--- web/next.config.js
+++ web/next.config.js
@@ -17,10 +17,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true, // TODO: Remove this when you fix all the errors.
   },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
 }
 
 module.exports = nextConfig
