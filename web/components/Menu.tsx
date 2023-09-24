d7c0dee608620df208c25efeecaa7ff562abfcb6
Casper Kuijjer <casper@kuijjer.com>
2023-09-24 11:38:46 +0200

    chore: extract Menu

---
+++ web/components/Menu.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { css } from '@emotion/react'
+
+import { Layout } from './Layout'
+import { useKeypress } from '../utils/hooks'
+
+import MenuIcon from './icons/menu.svg'
+import CrossIcon from './icons/cross.svg'
+import { headerFont } from '../utils/theme'
+
+export const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const openMenu = () => {
+    setIsMenuOpen(true)
+  }
+
+  const closeMenu = () => {
+    setIsMenuOpen(false)
+  }
+
+  return (
+    <>
+      <MenuTextItem href="/about">About</MenuTextItem>
+      <MenuTextItem href="/statistics">Statistics</MenuTextItem>
+      <MenuButton onClick={openMenu} />
+      {isMenuOpen && <MenuModal onClose={closeMenu} />}
+    </>
+  )
+}
+
+const MenuItem = ({ href, children }) => (
+  <Link
+    href={href}
+    css={css`
+      font-size: 24px;
+      font-weight: 700;
+      color: var(--text-inverse-color);
+      padding: 12px 24px;
+      text-decoration: none;
+      text-align: center;
+    `}
+    className={headerFont.className}
+  >
+    {children}
+  </Link>
+)
+
+const MenuTextItem = ({ href, children }) => (
+  <Link
+    href={href}
+    css={css`
+      font-size: 18px;
+      font-weight: 700;
+      color: var(--text-inverse-color);
+      text-decoration: none;
+
+      @media (max-width: 600px) {
+        display: none;
+      }
+    `}
+    className={headerFont.className}
+  >
+    {children}
+  </Link>
+)
+
+const MenuButton = ({ onClick }) => (
+  <div
+    css={css`
+      width: 32px;
+      height: 32px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      box-sizing: border-box;
+      cursor: pointer;
+
+      @media (min-width: 600px) {
+        display: none;
+      }
+    `}
+    onClick={onClick}
+  >
+    <MenuIcon />
+  </div>
+)
+
+const MenuModal = ({ onClose }) => {
+  useKeypress('Escape', onClose)
+
+  return (
+    <div
+      css={css`
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        position: fixed;
+        background-color: var(--background-inverse-color);
+        z-index: 1;
+      `}
+      onClick={onClose}
+    >
+      <Layout>
+        <div
+          css={css`
+            position: relative; /* so the MenuCloseButton can position itself */
+            display: flex;
+            flex-direction: column;
+            padding-top: 64px;
+          `}
+        >
+          <MenuCloseButton onClick={onClose} />
+          <MenuItem href="/">Home</MenuItem>
+          <MenuItem href="/about">About</MenuItem>
+          <MenuItem href="/statistics">Statistics</MenuItem>
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+const MenuCloseButton = ({ onClick }) => (
+  <div
+    css={css`
+      position: absolute;
+      top: 28px;
+      right: 0;
+      width: 32px;
+      height: 32px;
+      box-sizing: border-box;
+      cursor: pointer;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+    onClick={onClick}
+  >
+    <CrossIcon color="var(--text-inverse-color)" />
+  </div>
+)
--- web/components/NavigationBar.tsx
+++ web/components/NavigationBar.tsx
@@ -3,25 +3,24 @@ import { css } from '@emotion/react'
 import Link from 'next/link'
 
 import SearchIcon from './icons/search.svg'
-import MenuIcon from './icons/menu.svg'
 import CrossIcon from './icons/cross.svg'
 
 import { headerFont } from '../utils/theme'
 import { TextFilter } from './TextFilter'
-import { Layout } from './Layout'
+import { Menu } from './Menu'
 
-import { useKeypress, useSearch } from '../utils/hooks'
+import { useSearch } from '../utils/hooks'
 
 const Title = (props) => (
   <h1
-    css={css({
-      fontSize: 24,
-      marginTop: 0,
-      marginBottom: 4,
-      fontStyle: 'normal',
-      fontWeight: 700,
-      lineHeight: 'normal',
-    })}
+    css={css`
+      font-size: 24px;
+      margin-top: 0;
+      margin-bottom: 4px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `}
     className={headerFont.className}
     {...props}
   />
@@ -29,117 +28,48 @@ const Title = (props) => (
 
 const SubTitle = (props) => (
   <h2
-    css={css({
-      fontSize: 14,
-      fontWeight: 400,
-      marginTop: 0,
-      marginBottom: 0,
-      color: 'var(--text-inverse-muted-color)',
-    })}
+    css={css`
+      font-size: 14px;
+      font-weight: 400;
+      margin-top: 0;
+      margin-bottom: 0;
+      color: var(--text-inverse-muted-color);
+    `}
     {...props}
   />
 )
 
-const IconButton = (props) => (
-  <div
-    css={css({
-      width: 32,
-      height: 32,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      boxSizing: 'border-box',
-      cursor: 'pointer',
-    })}
-    {...props}
-  />
-)
-
-const Menu = ({ onClose }) => {
-  useKeypress('Escape', onClose)
+const SearchButton = ({ hidden, onClick, isSearchOpen }) => {
+  if (hidden) return null
 
   return (
     <div
-      css={css({
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        position: 'fixed',
-        backgroundColor: 'var(--background-inverse-color)',
-        zIndex: 1,
-      })}
-      onClick={onClose}
+      css={css`
+        width: 32px;
+        height: 32px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-sizing: border-box;
+        cursor: pointer;
+      `}
+      onClick={onClick}
     >
-      <Layout>
-        <div
-          css={css({
-            position: 'relative', // so the MenuCloseButton can position itself
-            display: 'flex',
-            flexDirection: 'column',
-            paddingTop: 64,
-          })}
-        >
-          <MenuCloseButton onClose={onClose} />
-          <MenuItem href="/">Home</MenuItem>
-          <MenuItem href="/about">About</MenuItem>
-          <MenuItem href="/statistics">Statistics</MenuItem>
-        </div>
-      </Layout>
+      {isSearchOpen ? <CrossIcon /> : <SearchIcon />}
     </div>
   )
 }
 
-const MenuCloseButton = ({ onClose }) => (
-  <div
-    css={css({
-      position: 'absolute',
-      top: 28,
-      right: 0,
-      width: 32,
-      height: 32,
-      boxSizing: 'border-box',
-      cursor: 'pointer',
-    })}
-  >
-    <CloseButton onClose={onClose} />
-  </div>
-)
-
-const CloseButton = ({ onClose }) => (
-  <IconButton onClick={onClose}>
-    <CrossIcon color="var(--text-inverse-color)" />
-  </IconButton>
-)
-
-const MenuItem = ({ href, children }) => (
-  <Link
-    href={href}
-    css={css({
-      fontSize: 24,
-      fontWeight: 700,
-      color: 'var(--text-inverse-color)',
-      padding: '12px 24px',
-      cursor: 'pointer',
-      textDecoration: 'none',
-      textAlign: 'center',
-    })}
-    className={headerFont.className}
-  >
-    {children}
-  </Link>
-)
-
 const Container = (props) => (
   <div
-    css={css({
-      display: 'flex',
-      alignItems: 'center',
-      paddingTop: 16,
-      paddingBottom: 16,
-      color: 'var(--text-inverse-color)',
-      backgroundColor: 'var(--background-inverse-color)',
-    })}
+    css={css`
+      display: flex;
+      align-items: center;
+      padding-top: 16px;
+      padding-bottom: 16px;
+      color: var(--text-inverse-color);
+      background-color: var(--background-inverse-color);
+    `}
     {...props}
   />
 )
@@ -158,19 +88,19 @@ const TitleContainer = (props) => (
   />
 )
 
-const IconContainer = (props) => (
+const NavigationItems = (props) => (
   <div
-    css={css({
-      flex: 0,
-      display: 'flex',
-      gap: 16,
-    })}
+    css={css`
+      flex: 0;
+      display: flex;
+      gap: 16px;
+      align-items: center;
+    `}
     {...props}
   />
 )
 
 export const NavigationBar = ({ showSearch = true }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const [isSearchOpen, setIsSearchOpen] = useState(false)
   const { search, setSearch } = useSearch()
 
@@ -178,14 +108,6 @@ export const NavigationBar = ({ showSearch = true }) => {
     const isOpen = search !== ''
     setIsSearchOpen(isOpen)
   }, [search])
-
-  const openMenu = () => {
-    setIsMenuOpen(true)
-  }
-
-  const closeMenu = () => {
-    setIsMenuOpen(false)
-  }
 
   const openSearch = () => {
     setIsSearchOpen(true)
@@ -223,18 +145,15 @@ export const NavigationBar = ({ showSearch = true }) => {
             </Link>
           )}
         </TitleContainer>
-        <IconContainer>
-          {showSearch && (
-            <IconButton onClick={toggleSearch}>
-              {isSearchOpen ? <CrossIcon /> : <SearchIcon />}
-            </IconButton>
-          )}
-          <IconButton onClick={openMenu}>
-            <MenuIcon />
-          </IconButton>
-        </IconContainer>
+        <NavigationItems>
+          <SearchButton
+            hidden={!showSearch}
+            isSearchOpen={isSearchOpen}
+            onClick={toggleSearch}
+          />
+          <Menu />
+        </NavigationItems>
       </Container>
-      {isMenuOpen && <Menu onClose={closeMenu} />}
     </>
   )
 }
