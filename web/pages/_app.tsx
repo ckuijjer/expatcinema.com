cd7a94133d103757d5401f83b0962f187ada63c4
Casper Kuijjer <casper@kuijjer.com>
2023-11-11 15:37:49 +0100

    🤞GA4 in _app instead of _document

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,15 +1,37 @@
 import type { AppProps } from 'next/app'
 import { Global } from '@emotion/react'
 import { Settings } from 'luxon'
+import Script from 'next/script'
 
 import { globalStyles } from '../utils/theme'
 
 Settings.defaultZone = 'Europe/Amsterdam'
 
+const GA_TRACKING_ID = 'G-W3YQQJKL1T'
+
+const GoogleAnalytics = () => (
+  <>
+    <Script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+    />
+    <Script id="google-analytics">
+      {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', ${GA_TRACKING_ID});
+  `}
+    </Script>
+  </>
+)
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={globalStyles} />
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </>
   )
