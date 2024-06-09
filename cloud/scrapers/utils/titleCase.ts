d5005467fc8edbcb4996cdd2b88c69cf6fac97a9
Casper Kuijjer <casper@kuijjer.com>
2024-06-09 20:57:34 +0200

    regex to do split using \b, but with more unicode support

--- cloud/scrapers/utils/titleCase.ts
+++ cloud/scrapers/utils/titleCase.ts
@@ -1,5 +1,8 @@
 const MINOR_WORDS = 'a an and as at by for from in of on or the to with'
 
+// Basically \b with better unicode support, see https://stackoverflow.com/a/57290540/65971
+const UNICODE_BOUNDARY = /(?<=\p{L})(?=\P{L})|(?<=\P{L})(?=\p{L})/u
+
 const isRomanNumeral = (word: string) => {
   return /^[IVXLCDM]+$/.test(word) // FIXME: This is not correct, e.g. 'IXVI' is not a valid roman numeral
 }
@@ -16,7 +19,7 @@ export const titleCase = (input: string) => {
   const listOfMinorWords = MINOR_WORDS.split(' ').map((w) => w.toLowerCase())
 
   return input
-    .split(/\b/)
+    .split(UNICODE_BOUNDARY)
     .map((word, i) => {
       if (isAllSpaces(word)) {
         return ' '
