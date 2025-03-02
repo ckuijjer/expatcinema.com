cd5ebad8c399985a708fcf07c9f971d75e7aeca4
Casper Kuijjer <casper@kuijjer.com>
2025-03-02 15:03:43 +0100

    use DYNAMODB_ANALYTICS from GitHub secrets import analytics from cloud

--- backend/src/analytics.ts
+++ backend/src/analytics.ts
@@ -1,11 +1,11 @@
-// import { analytics } from '../../cloud/handler'
+import { analytics } from '../../cloud/handler'
 
-// export const handler = analytics
+export const handler = analytics
 
-export const handler = async (event: any) => {
-  console.log('Event: ', event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Lambda!' }),
-  }
-}
+// export const handler = async (event: any) => {
+//   console.log('Event: ', event)
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: 'Hello from Lambda!' }),
+//   }
+// }
