6cf4491f8dfbb7f61e4f970a47aa0f386732f7a7
Casper Kuijjer <casper@kuijjer.com>
2025-03-02 10:35:06 +0100

    rename, import from cloud

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib'
 import { BackendStack } from '../lib/backend-stack'
 
 const app = new cdk.App()
-new BackendStack(app, 'expatcinema-backend', {
+new BackendStack(app, 'expatcinema', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
---
+++ backend/src/analytics.ts
@@ -0,0 +1,3 @@
+import { analytics } from '../../cloud/handler'
+
+export const handler = analytics
