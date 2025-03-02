9d9142b63b97aeed2f799d4c0a38242d74658f80
Casper Kuijjer <casper@kuijjer.com>
2025-03-02 11:22:44 +0100

    use tsx instead of ts-node for running cdk

--- backend/lib/config.ts
+++ backend/lib/config.ts
@@ -1,9 +1,3 @@
-import * as dotenv from 'dotenv'
-
-import path = require('path')
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
-
 export type ConfigProps = {
   DYNAMODB_ANALYTICS: string
 }
