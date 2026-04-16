2e99de67b055e11e5612659afd9fa297a5f31d72
Casper Kuijjer <casper@kuijjer.com>
2026-04-16 08:51:53 +0200

    Link screening posters to canonical movie pages

--- web/components/Calendar/DirectCalendar.tsx
+++ web/components/Calendar/DirectCalendar.tsx
@@ -7,26 +7,16 @@ import { ScreeningRow } from '../Screening'
 export const DirectCalendar = ({
   rows,
   showCity,
-  currentCity,
-  currentCinema,
 }: {
   rows: Row[]
   showCity: boolean
-  currentCity?: string
-  currentCinema?: string
 }) => (
   <>
     {rows.map((row, i) =>
       row.component === 'RelativeDate' ? (
         <RelativeDate key={i} {...row.props} />
       ) : (
-        <ScreeningRow
-          key={i}
-          {...row.props}
-          showCity={showCity}
-          currentCity={currentCity}
-          currentCinema={currentCinema}
-        />
+        <ScreeningRow key={i} {...row.props} showCity={showCity} />
       ),
     )}
   </>
